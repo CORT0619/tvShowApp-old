e7d0d19de76aabda428c9ef8920d840e5625c6ab
Crystal R. Williams <cortney0215@gmail.com>
2019-04-08 20:14:34 -0400

    add bookmarks, search, and dashboard pages

---
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.page.html',
+  styleUrls: ['./dashboard.page.scss'],
+})
+export class DashboardPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  openMenu() {
+    console.log('opening...');
+  }
+
+}
