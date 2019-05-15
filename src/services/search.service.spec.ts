5ee5d9ce4eae7690d876b92652bf3c605fca7756
ccmc-cwilliams <cwilliams@ccmcinc.com>
2019-05-15 14:21:23 -0400

    fix errors with all tests

--- src/services/search.service.spec.ts
+++ src/services/search.service.spec.ts
@@ -1,12 +1,24 @@
-import { TestBed } from '@angular/core/testing';
-
 import { SearchService } from './search.service';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from './config.service';
+import { of, throwError } from 'rxjs';
 
 describe('SearchService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: SearchService;
+  let mockHttpClient: jasmine.SpyObj<HttpClient>;
+  let mockConfigService: jasmine.SpyObj<ConfigService>;
+
+  beforeEach(() => {
+    mockHttpClient = jasmine.createSpyObj('HttpClient', ['get']);
+    mockHttpClient.get.and.returnValue(of({}));
+
+    mockConfigService = jasmine.createSpyObj('ConfigService', ['handleError']);
+    mockConfigService.handleError.and.returnValue(throwError('Some error!'));
+
+    service = new SearchService(mockHttpClient, mockConfigService);
+  });
 
   it('should be created', () => {
-    const service: SearchService = TestBed.get(SearchService);
     expect(service).toBeTruthy();
   });
 });
